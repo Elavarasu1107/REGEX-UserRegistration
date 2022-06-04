c14a2ea14f84e81feac6fb39cee024afaac354df
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-04 11:47:35 +0530

    [Elavarasu] UC1-FirstName

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Your First Name: ");
+            string firstName = Console.ReadLine();
+            User getMethod = new User();
+            getMethod.NameChecker(firstName);
+        }
+    }
+}
---
+++ UserRegistration/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class User
+    {
+        public void NameChecker(string inputName)
+        {
+            const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+            if(Regex.IsMatch(inputName, NAME))
+            {
+                Console.WriteLine("Your Name is Valid");
+                return;
+            }
+            Console.WriteLine("Your Name is Invalid...Name should start with Caps and have atleast 3 characters");
+
+        }
+    }
+}
